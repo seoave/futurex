3038feb55dde81eac729be64e19ba171a0015475
Seoave <seo.ave@gmail.com>
2022-10-18 01:38:09 +0300

    Add Symfony

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController
+{
+    #[Route('/', name: 'home')]
+    public function homepage(): Response
+    {
+        return new Response('home');
+    }
+}
