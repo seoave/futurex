1c81732e2d4a5ac747c44da99952d3baa88f07b9
Seoave <seo.ave@gmail.com>
2022-10-20 23:08:59 +0300

    Update UserProfile

--- src/Controller/UserProfile.php
+++ src/Controller/UserProfile.php
@@ -8,9 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserProfile
 {
-    #[Route('user/{slug}')]
-    public function show(string $slug): Response
+    #[Route('user/profile/{slug}')]
+    public function view(string $slug): Response
     {
         return new Response('User profile ' . $slug);
+    }
+
+    #[Route('user/edit/{slug}')]
+    public function edit(string $slug): Response
+    {
+        return new Response('Edit user profile ' . $slug);
+    }
+
+    #[Route('/api/v1/json/user/{slug}')]
+    public function get(string $slug): Response
+    {
+        return new Response('Get user data ' . $slug);
     }
 }
