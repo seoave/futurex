144b252df50ea370dfcd7bf947b2af6ea378bda0
Seoave <seo.ave@gmail.com>
2022-11-06 03:40:23 +0200

    Add templates

---
+++ src/Controller/RegistrationPageController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationPageController extends AbstractController
+{
+    #[Route('/registration', name: 'registration_page')]
+    public function registration(): Response
+    {
+        return $this->render('registration.html.twig', [
+            'title' => 'Registration',
+        ]);
+    }
+}
