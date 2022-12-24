3fcb4ce390588431543f38f74af360111c3c992f
Oleksandr Burkhan <seo.ave@gmail.com>
2022-12-24 15:36:01 +0200

    Update compose settings

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -8,13 +8,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\String\UnicodeString;
+use Symfony\Contracts\Cache\CacheInterface;
+
 use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
+
+    public function __construct(
+       private CacheInterface $cache
+    ) {
+
+    }
+
     #[Route('/vinyls/{slug}', name: 'vinyls_beatles')]
     public function show(?string $slug = null): Response
     {
+        dd($this->cache->get('vinyls_' . $slug, function () use ($slug){
+           return new UnicodeString($slug);
+        }));
+
         if ($slug !== null) {
             $title = u(str_replace('-', ' ', $slug))->title(allWords: true);
         } else {
