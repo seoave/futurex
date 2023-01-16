fafaddb906778ce03290e3a70069eabf6e1e6fa8
Oleksandr Burkhan <seo.ave@gmail.com>
2023-01-17 01:36:58 +0200

    Add all user funds output method

--- src/Controller/Wallet/WalletController.php
+++ src/Controller/Wallet/WalletController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Wallet;
 
+use App\Repository\WalletRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class WalletController extends AbstractController
 {
     #[Route('/my/wallet', name: 'app_user_wallet_view')]
-    public function view(): Response
+    public function view(WalletRepository $repository): Response
     {
+        $userId = 9;
+        $currencies = $repository->findAllUserFunds($userId);
+        $tokens = $repository->findAllUserFunds($userId, true);
+
         return $this->render('wallet/index.html.twig', [
             'title' => 'My Wallet',
-            'currencies' => [],
-            'tokens' => [],
+            'currencies' => $currencies,
+            'tokens' => $tokens,
         ]);
     }
 }
