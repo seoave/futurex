50821e30b3248a91071abbef4aecf44543b58135
Oleksandr Burkhan <seo.ave@gmail.com>
2023-01-22 01:59:17 +0200

    Update Checkout template

--- src/Controller/Payment/CheckoutController.php
+++ src/Controller/Payment/CheckoutController.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\Controller\Payment;
 
+use App\Repository\OfferRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +12,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class CheckoutController extends AbstractController
 {
     #[Route('/order/checkout/match-{match}/actual-{actual}', name: 'app_payment_checkout_index')]
-    public function index(int $match, int $actual): Response
+    public function index(int $match, int $actual, OfferRepository $repository): Response
     {
+        $matchOffer = $repository->find($match);
+        $actualOffer = $repository->find($actual);
+
         return $this->render('payment/checkout.html.twig', [
             'title' => 'Checkout',
-            'match' => $match,
-            'actual' => $actual,
+            'matchOffer' => $matchOffer,
+            'actualOffer' => $actualOffer,
         ]);
     }
 }
