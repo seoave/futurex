09ae4681f6495d266f662b4794e16e978dc8806c
Oleksandr Burkhan <seo.ave@gmail.com>
2023-01-30 07:21:24 +0200

    Add RegistrationController

--- src/Controller/User/RegistrationController.php
+++ src/Controller/User/RegistrationController.php
@@ -2,7 +2,18 @@
 declare(strict_types=1);
 
 namespace App\Controller\User;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 class RegistrationController extends AbstractController
 {
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher
+    ) {
+    }
+
+    public function index()
+    {
+    }
 }
