4f5207984f0c2c8fd82758f747a192109564ebfe
Oleksandr Burkhan <seo.ave@gmail.com>
2023-01-30 06:55:16 +0200

    Add Security bundle, update User entity

---
+++ src/Controller/User/RegistrationController.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class RegistrationController extends AbstractController
+{
+}
