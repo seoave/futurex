47f6dc32801f0135f3bae38574d76e7e4a967b33
root <root@DESKTOP-K5G35RD>
2023-02-04 13:38:25 +0200

    Use home widgets

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,17 +4,26 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home_homepage')]
-    public function homepage(): Response
+    public function __construct(
+        private readonly UserRepository $userRepository
+    ) {
+    }
+
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
     {
-        return $this->render('base.html.twig', [
+        $traders = $this->userRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
             'title' => 'FUTUREX',
+            'traders' => ! empty($traders) ? $traders : [],
         ]);
     }
 }
